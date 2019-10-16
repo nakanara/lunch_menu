fceb6f8eb1fcbe94b4e6430c94994d5d33313c77
NaKaNaRa <nakanara@naver.com>
2019-10-16 16:59:39 +0900

    init

---
+++ server.js
@@ -0,0 +1,48 @@
+// server.js
+
+var express    = require('express');
+var app        = express();
+var path       = require('path');
+var mongoose   = require('mongoose');
+var bodyParser = require('body-parser');
+const config = require('./config.json');
+
+
+var MONGO_DB_URI = config['test']['MONGODB_URI'];
+
+// Database
+mongoose.Promise = global.Promise;
+//mongoose.connect(process.env.MONGO_DB, {useMongoClient: true});
+const dbconfig = {
+  autoIndex: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(MONGO_DB_URI, dbconfig);
+var db = mongoose.connection;
+db.once('open', function () {
+   console.log('DB connected!');
+});
+db.on('error', function (err) {
+  console.log('DB ERROR:', err);
+});
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(function (req, res, next) { //1
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'content-type');
+  next();
+});
+
+// API
+app.use('/api/heroes', require('./api/heroes'));
+
+// Server
+var port = 3000;
+app.listen(port, function(){
+  console.log('listening on port:' + port);
+});
