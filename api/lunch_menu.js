22192990279a40afc45c92c60b6ab897e00cb0d8
nakanara <nakanara@naver.com>
2019-10-22 12:47:04 +0900

    test

--- api/lunch_menu.js
+++ api/lunch_menu.js
@@ -3,13 +3,13 @@ var express     = require('express');
 var router      = express.Router();
 var Store       = require('../models/store');
 var mongoose    = require('mongoose');
+var $           = require('jquery');
 
 // Index
 router.get('/',
   function(req, res, next){
     var query = {};
     if(req.query.name) query.name = {$regex:req.query.name, $options:'i'};
-
     Store.find(query)
       .sort({id:1})
       .exec(function(err, stores){
@@ -20,10 +20,11 @@ router.get('/',
           res.json({success:true, data:stores});
         }
       })
-  })
+  }
+)
 
 // Show
-router.get('/:id',
+router.get('/store/:id',
 function(req, res, next){
   Store.findOne({id:req.params.id})
   .exec(function(err, store){
@@ -111,4 +112,84 @@ function(req, res, next){
 }
 );
 
+
+router.get('/ramdom', 
+  function(req, res, next){
+    Store.findOne({})
+    .sort({id: -1})
+    .exec(function(err, store){
+      if(err) {
+        res.status(500);
+        return res.json({success:false, message:err});
+      }
+      else {
+        res.locals.lastId = store?store.id:0;
+        next();
+      }
+    });
+  },
+  function(req, res, next) {
+    var limit =5;
+    // 삭제되었을 때를 위한 여분
+    var idxs = getRamdon((limit*2), res.locals.lastId);
+    var nCount = 0;
+    Store.find({id:{$in:idxs}})
+      .exec(function(err, stores){
+        if(err) {
+          res.status(500);
+          res.json({success:false, message:err});
+        } else {
+
+          var result = [];
+          idxs.every((num, idx) => {
+
+            stores.every((sitem, si)  => {
+              if(sitem['id'] == num) {
+                result.push(sitem);
+                nCount++;
+                return false;
+              }
+              return true;
+            })
+
+            console.log('nCount=' + nCount +'/limit=' + limit);
+            if(nCount == limit) return false;
+            return true;
+          });
+          res.json({success:true, idx:idxs, data:result});
+        }
+      });
+  }
+)
+
+/**
+ * 중복되지 않는 난수 생성
+ * @param {*} size 전체 수
+ * @param {*} maxIdx 최대 값
+ */
+function getRamdon(size, maxIdx) {
+  let result = [];
+  let isRun = true;
+  let nCount = 0;
+  let returnResult = [];
+
+  if(size > maxIdx) {
+    size = maxIdx;
+  }
+
+  do{
+    let v = Math.round(Math.random() * maxIdx);
+    v += 1; 
+
+    if(result[v] != 1) {
+      result[v] = 1;
+      nCount++;
+      returnResult.push(v);
+
+      if(nCount == size) isRun = false;
+    }
+  }while(isRun);
+
+  return returnResult;
+}
 module.exports = router;
--- public/map.js
+++ public/map.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
 
+  var map_markers = [];
+  var map ;
+  var reg_marker;
+  
   {
     // test
     $('#inputName').val('스타벅스');
     $('#inputTags').val('커피 음료');
     $('#inputDesc').val('2층 복잡함');
+
+
   }
+
+  mapInit('map');
 
   {
     $('#btnSearch').on('click', function(e){
+
+      clearMarker();
+
       $.ajax({
         url: '/api/lunch_menu/',
         data: {},
@@ -22,7 +33,52 @@ $(document).ready(function(){
 
           let html ='';
           $.each(data.data, function(idx, item){
-            html += '<tr><td>'+item.name+'</td><td><i class="fas fa-award"></i><i class="far fa-thumbs-up"></i><i class="far fa-thumbs-down"></i>'+item.id+'</td></tr>';
+            html += '<tr><td>'+(idx+1)+'</td><td>'+item.name+'</td><td><i class="fas fa-award"></i><i class="far fa-thumbs-up"></i><i class="far fa-thumbs-down"></i>'+item.id+'</td></tr>';
+      
+            // 마커가 표시될 위치입니다 
+            var markerPosition = new kakao.maps.LatLng(item.lat, item.lng); 
+
+            // 마커를 생성합니다
+            var marker = new kakao.maps.Marker({
+                position: markerPosition
+            });
+
+            addMarker(marker);
+          });
+          $('#resultTable tbody').html(html); 
+        }
+      })
+    });
+
+    $('#btnRSearch').on('click', function(e){
+      
+      clearMarker();
+      $.ajax({
+        url: '/api/lunch_menu/ramdom',
+        data: {},
+        method: 'get', 
+        error: function(xhr, status, error){
+          console.log(status);
+        },
+        success: function(data, status, xhr) {
+          console.log(data);
+          console.log(status);
+
+          let html ='';
+          
+          $.each(data.data, function(idx, item){
+            html += '<tr><td>'+(idx+1)+'</td><td>'+item.name+'</td><td><i class="fas fa-award"></i><i class="far fa-thumbs-up"></i><i class="far fa-thumbs-down"></i>'+item.id+'</td></tr>';
+
+            // 마커가 표시될 위치입니다 
+            var markerPosition = new kakao.maps.LatLng(item.lat, item.lng); 
+
+            // 마커를 생성합니다
+            var marker = new kakao.maps.Marker({
+                position: markerPosition
+            });
+
+            addMarker(marker);
+
           });
           $('#resultTable tbody').html(html); 
         }
@@ -48,43 +104,76 @@ $(document).ready(function(){
       return false;
     });
   }
-  var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-  var options = { //지도를 생성할 때 필요한 기본 옵션
-      //center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      center: new kakao.maps.LatLng(37.489111, 127.007998),
-      level: 3 //지도의 레벨(확대, 축소 정도)
-  };
 
-  var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+  function addMarker(marker) {
+    marker.setMap(map);
+    map_markers.push(marker);
+  }
 
-  // 지도를 클릭한 위치에 표출할 마커입니다
-  var marker = new kakao.maps.Marker({ 
-    // 지도 중심좌표에 마커를 생성합니다 
-    position: map.getCenter() 
-  }); 
-  // 지도에 마커를 표시합니다
-  marker.setMap(map);
+  function clearMarker() {
+    for(var _m in map_markers) {
+      map_markers.forEach(function( _m, idx) {
+        _m.setMap(null);
+      });
+      map_markers = [];
+    }
+  }
 
 
+  function mapInit(mapId){
+    var container = document.getElementById(mapId); //지도를 담을 영역의 DOM 레퍼런스
+    var options = { //지도를 생성할 때 필요한 기본 옵션
+        //center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+        center: new kakao.maps.LatLng(37.489111, 127.007998),
+        level: 3 //지도의 레벨(확대, 축소 정도)
+    };
+  
+    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+  
+    // 지도를 클릭한 위치에 표출할 마커입니다
+    var marker = new kakao.maps.Marker({ 
+      // 지도 중심좌표에 마커를 생성합니다 
+      position: map.getCenter() 
+    }); 
+    // 지도에 마커를 표시합니다
+    marker.setMap(map);
+  
+    
+  
+  
+    // 지도에 클릭 이벤트를 등록합니다
+    // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+    kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
+  
+      // 클릭한 위도, 경도 정보를 가져옵니다 
+      var latlng = mouseEvent.latLng;
+  
+      var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
+      message += '경도는 ' + latlng.getLng() + ' 입니다';
+  
+      // 마커 위치를 클릭한 위치로 옮깁니다
+    
+      if(reg_marker) {
+        reg_marker.setPos
+      } else {
+        // 지도를 클릭한 위치에 표출할 마커입니다
+        reg_marker = new kakao.maps.Marker({ 
+          // 지도 중심좌표에 마커를 생성합니다 
+          position: map.getCenter() 
+        }); 
+        // 지도에 마커를 표시합니다
+        reg_marker.setMap(map);
+      }
 
-  // 지도에 클릭 이벤트를 등록합니다
-  // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
-  kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
+      
+      $('#lat').val(latlng.getLat());
+      $('#lng').val(latlng.getLng());
+  
+      var resultDiv = document.getElementById('result'); 
+      resultDiv.innerHTML = message;
+  
+    });
+  }
 
-    // 클릭한 위도, 경도 정보를 가져옵니다 
-    var latlng = mouseEvent.latLng;
-
-    var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
-    message += '경도는 ' + latlng.getLng() + ' 입니다';
-
-    // 마커 위치를 클릭한 위치로 옮깁니다
-    marker.setPosition(latlng);
-
-    $('#lat').val(latlng.getLat());
-    $('#lng').val(latlng.getLng());
-
-    var resultDiv = document.getElementById('result'); 
-    resultDiv.innerHTML = message;
-
-  });
+  
 });
