c3c820b75842924208428cb4e55bd1972efe50bd
nakanara <nakanara@naver.com>
2019-11-16 11:09:37 +0900

    test

--- public/map.js
+++ public/map.js
@@ -3,17 +3,34 @@ $(document).ready(function(){
   var map_markers = [];
   var map ;
   var reg_marker;
+  var resultData = [];
   
   {
     // test
     $('#inputName').val('스타벅스');
     $('#inputTags').val('커피 음료');
     $('#inputDesc').val('2층 복잡함');
+    fnRandomSearch();
+    mapInit('map');
 
+
+    Vue.component('my-title', {
+      template: '<div>{{webtitle}}</div>',
+      data : {
+        tabledata : resultData
+      },
+      /*function*/
+    });
+
+    let app = new Vue({
+      el: '#vue',
+      data: {
+        title: 'Hello Map'
+      }
+    })
 
   }
 
-  mapInit('map');
 
   {
     $('#btnSearch').on('click', function(e){
@@ -32,6 +49,7 @@ $(document).ready(function(){
           console.log(status);
 
           let html ='';
+          resultData = data.data;
           $.each(data.data, function(idx, item){
             html += '<tr><td>'+(idx+1)+'</td><td>'+item.name+'</td><td><i class="fas fa-award"></i><i class="far fa-thumbs-up"></i><i class="far fa-thumbs-down"></i>'+item.id+'</td></tr>';
       
@@ -51,38 +69,15 @@ $(document).ready(function(){
     });
 
     $('#btnRSearch').on('click', function(e){
-      
-      clearMarker();
-      $.ajax({
-        url: '/api/lunch_menu/ramdom',
-        data: {},
-        method: 'get', 
-        error: function(xhr, status, error){
-          console.log(status);
-        },
-        success: function(data, status, xhr) {
-          console.log(data);
-          console.log(status);
+      fnRandomSearch();
+    });
 
-          let html ='';
-          
-          $.each(data.data, function(idx, item){
-            html += '<tr><td>'+(idx+1)+'</td><td>'+item.name+'</td><td><i class="fas fa-award"></i><i class="far fa-thumbs-up"></i><i class="far fa-thumbs-down"></i>'+item.id+'</td></tr>';
+    $('#btnAdd').on('click', function(){
+      $('#inputName').val('').focus();
+      $('#inputTags').val('');
+      $('#inputDesc').val('');
+      $('#f').show();
 
-            // 마커가 표시될 위치입니다 
-            var markerPosition = new kakao.maps.LatLng(item.lat, item.lng); 
-
-            // 마커를 생성합니다
-            var marker = new kakao.maps.Marker({
-                position: markerPosition
-            });
-
-            addMarker(marker);
-
-          });
-          $('#resultTable tbody').html(html); 
-        }
-      })
     });
 
     $('#btnSave').on('click', function(e){
@@ -103,6 +98,40 @@ $(document).ready(function(){
       });
       return false;
     });
+  }
+
+  function fnRandomSearch(){
+    clearMarker();
+    $.ajax({
+      url: '/api/lunch_menu/ramdom',
+      data: {},
+      method: 'get', 
+      error: function(xhr, status, error){
+        console.log(status);
+      },
+      success: function(data, status, xhr) {
+        console.log(data);
+        console.log(status);
+
+        let html ='';
+        
+        $.each(data.data, function(idx, item){
+          html += '<tr><td>'+(idx+1)+'</td><td>'+item.name+'</td><td><i class="fas fa-award"></i><i class="far fa-thumbs-up"></i><i class="far fa-thumbs-down"></i>'+item.id+'</td></tr>';
+
+          // 마커가 표시될 위치입니다 
+          var markerPosition = new kakao.maps.LatLng(item.lat, item.lng); 
+
+          // 마커를 생성합니다
+          var marker = new kakao.maps.Marker({
+              position: markerPosition
+          });
+
+          addMarker(marker);
+
+        });
+        $('#resultTable tbody').html(html); 
+      }
+    })
   }
 
   function addMarker(marker) {
@@ -131,12 +160,12 @@ $(document).ready(function(){
     var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
   
     // 지도를 클릭한 위치에 표출할 마커입니다
-    var marker = new kakao.maps.Marker({ 
+    reg_marker = new kakao.maps.Marker({ 
       // 지도 중심좌표에 마커를 생성합니다 
       position: map.getCenter() 
     }); 
     // 지도에 마커를 표시합니다
-    marker.setMap(map);
+    reg_marker.setMap(map);
   
     
   
@@ -154,7 +183,7 @@ $(document).ready(function(){
       // 마커 위치를 클릭한 위치로 옮깁니다
     
       if(reg_marker) {
-        reg_marker.setPos
+        reg_marker.setPosition(latlng);
       } else {
         // 지도를 클릭한 위치에 표출할 마커입니다
         reg_marker = new kakao.maps.Marker({ 
@@ -177,3 +206,4 @@ $(document).ready(function(){
 
   
 });
+
